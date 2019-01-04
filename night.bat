112f92ac1c00b4e3ea7b96aeccbecab672634699
ooozbh <ooozbh@gmail.com>
2019-01-05 00:07:50 +0800

    sth

--- night.bat
+++ night.bat
@@ -1,3 +1,7 @@
 python3 ./main.py y n 1
-python3 ./main.py y n 1
-
+python3 ./main.py n n 1
+python3 ./main.py y y 1
+python3 ./main.py n y 1
+python3 ./main.py y n 3
+python3 ./main.py n n 3
+python3 ./main.py y y 3
