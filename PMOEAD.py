8cc1c58492a75b91baa0c5a847bd51e481061c39
Kaiqiang2 <11610325@mail.sustc.edu.cn>
2019-01-04 20:30:48 +0800

    Add run by time model.

--- PMOEAD.py
+++ PMOEAD.py
@@ -1,5 +1,5 @@
 from Initial import Initial
-import random
+import random, time
 from Variation import CrossOver
 from evaluate_solution import evaluate_singlefitness, evaluate_solution, evaluate_single
 import numpy
@@ -10,6 +10,30 @@ def PMOEAD(file_name, dimension, population_size, max_iteration, begin, end):
     iteration = 0
     negihbour_num = len(neighbours[0])
     while iteration < max_iteration:
+        iteration += 1
+        index = begin
+        while index < end:
+            p = random.sample(range(0, negihbour_num), 2)  # select two parents from its neighbour
+            p1 = int(neighbours[index][p[0]])
+            p2 = int(neighbours[index][p[1]])
+            indiviual = CrossOver(population[p1], population[p2], dimension)
+            i_obj = evaluate_single(indiviual,copy(data))
+            if i_obj[0] < z[0]:
+                z[0] = i_obj[0]
+            if i_obj[1] < z[1]:
+                z[1] = i_obj[1]
+            update_neighbour(population, neighbours[index], indiviual, obj, fitness, weight_vecotr)
+            index += 1
+        # print(fiteration {iteration}'
+        print("iteration:", iteration)
+    return population, obj
+
+def PMOEAD_bytime(file_name, dimension, population_size, max_time, begin, end):
+    TIME = time.time()
+    population, weight_vecotr, neighbours, obj, z, fitness, data = Initial(population_size, dimension, file_name)
+    iteration = 0
+    negihbour_num = len(neighbours[0])
+    while time.time()-TIME<max_time:
         iteration += 1
         index = begin
         while index < end:
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from PMOEAD import PMOEAD
+from PF import get_pf
+from PMOEAD import PMOEAD,PMOEAD_bytime
 #from PF import get_pf
 from store_result import store_result
-from controller import parallel_run
+from controller import parallel_run, parallel_run_bytime
 import time
 
 if __name__ == '__main__':
@@ -13,8 +14,33 @@ if __name__ == '__main__':
 
     begin = 0
     end = population_size
-    #population, obj = PMOEAD(file_name, feature_num, population_size, iteration_num, begin, end)
-    population, obj = parallel_run(10, 10, 12, file_name, 166, population_size)
-    store_result(obj, file_name, population_size, iteration_num)
-    #get_pf(obj, file_name, population_size, iteration_num)
+    #------ This is the single core--------
+    # population, obj = PMOEAD(file_name=file_name, dimension=feature_num, population_size=population_size,
+                             # max_iteration=iteration_num, begin=begin, end=end)
+    # ------------------------------------
+
+    # -----------This is the multiple cores
+    # round, iteration_num, cpu_num, file_name, dimension, population_size
+    # population, obj = parallel_run(round=10, iteration_num=10, cpu_num=8, file_name=file_name, dimension=166, population_size=population_size)
+    # ------------------------------------
+
+    # -------------Run by time parallel----------------------
+    # max_time,iteration_num, cpu_num, file_name, dimension, population_size
+    # run_time =200
+    # population, obj = parallel_run_bytime(max_time=run_time, iteration_num=10, cpu_num=8, file_name=file_name, dimension=166, population_size=population_size)
+    # file_name = file_name +'-parallel run by time'
+    # store_result(obj, file_name, population_size, run_time)
+    # ------------------------------------------
+
+    # ----------------Run by time single core---------------
+    # file_name, dimension, population_size, max_time, begin, end
+    run_time =200
+    population, obj = PMOEAD_bytime(file_name=file_name, dimension=feature_num, population_size=population_size, max_time=run_time, begin=begin, end=end)
+    file_name = file_name +'-single core run by time'
+    store_result(obj, file_name, population_size, run_time)
+
+    # --------------------------------------------------------
+
+    # store_result(obj, file_name, population_size, iteration_num)
+    get_pf(obj, file_name, population_size, iteration_num)
     print(time.time()-begin_time)
